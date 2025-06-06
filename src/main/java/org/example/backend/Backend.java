ab0b9303e317d441f6470cea566a97e4399daaf8
MariPinas <marianapls2003@gmail.com>
2025-06-06 20:05:48 -0300

    :sparkles: add backend class

--- src/main/java/org/example/backend/Backend.java
+++ src/main/java/org/example/backend/Backend.java
@@ -1,4 +1,16 @@
 package org.example.backend;
 
-public class Backend {
+/**
+ * Implementação concreta do XML processor. Simula um backend que só
+ * entende XML. Aqui imprimimos no console, mas pode haver outras chamadas de serviços.
+ */
+public class Backend implements XMLprocessor {
+
+    @Override
+    public void processXML(String xmlData) {
+        // Simulação de processamento:
+        System.out.println("[Backend] Recebeu XML para processar:");
+        System.out.println(xmlData);
+        System.out.println("[Backend] Processamento concluído.\n");
+    }
 }
